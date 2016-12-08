a4632cb91bd9d66f092cc2c4865118b05b8e51ac
Giacomo Rossetto <jackyman_cs4@live.it>
2016-12-08 17:20:47 +0100

    Added error_print() and improved output format.

--- ituneslibrarian/utils.py
+++ ituneslibrarian/utils.py
@@ -1,38 +1,37 @@
-# Create a stylesheet.
-error_style = style_from_dict({
-    Token.Error: '#ff0066 bold',
-    Token.Message: '#000000 italic',
-})
+from prompt_toolkit import prompt as pprompt
+from prompt_toolkit.shortcuts import print_tokens
+from prompt_toolkit.styles import style_from_dict
+from pygments.token import Token
 
-
-def promt(message, options_values, default_index):
+def prompt(message, options_values, default_index):
 
     if default_index != False:
         default_value = options_values[default_index]
-        default_print_message = " [default: " + str(default_value) +"]
+        default_print_message = " [default: " + str(default_value) + "] "
     else:
         default_value = False
-        default_print_message = ""
+        default_print_message = " "
 
     input_value = ""
 
     while input_value not in options_values:
-        input_value = prompt(
+        input_value = pprompt(
             message + str(options_values) + default_print_message)
 
         if default_value != False and input_value == "":
-            input_value=default_value
+            input_value = default_value
 
     return input_value
 
+
 def notice_print(message):
 
-    notice_style=style_from_dict({
+    notice_style = style_from_dict({
         Token.Notice: '#44ff44 bold',
         Token.Message: '#000000 italic',
     })
 
-    tokens=[
+    tokens = [
         (Token.Notice, 'Notice: '),
         (Token.Message, message),
         (Token, '\n'),
@@ -48,8 +47,22 @@ def warning_print(message):
     })
 
     tokens = [
-        (Token.Warning, 'Warning:'),
+        (Token.Warning, 'Warning: '),
         (Token.Message, message),
         (Token, '\n'),
     ]
     print_tokens(tokens, style=warning_style)
+
+def error_print(message):
+
+    error_style = style_from_dict({
+        Token.Error: '#ff0066 bold',
+        Token.Message: '#000000 italic',
+    })
+
+    tokens = [
+        (Token.Error, 'Error: '),
+        (Token.Message, message),
+        (Token, '\n'),
+    ]
+    print_tokens(tokens, style=error_style)
