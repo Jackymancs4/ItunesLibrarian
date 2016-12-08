91464b4e19beffe31d629159d79cafdef67dbe68
Giacomo Rossetto <jackyman_cs4@live.it>
2016-12-08 17:21:16 +0100

    Deleted useless libraries.

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@
 Usage:
   ituneslibrarian.py parse [-i <input>] [-o <output>] [-c]
   ituneslibrarian.py search -f <name> [-s] [-c] [-p | -n] [--no-album | --album=<album>] [--no-artist | --artist=<artist>] [--no-title | --title=<title>]
-  ituneslibrarian.py modify split [-b <symbol>] -f <name> [-s]
+  ituneslibrarian.py modify split [-b <symbol>] [-i <input>] [-s]
 
 Options:
   -i, --input <input>     [default: Library.xml]
@@ -15,17 +15,10 @@ Options:
   -c, --check           [default: false]
 
 """
-import os.path
-import urllib.request
 
+import ituneslibrarian.modify
 import ituneslibrarian.parse
 from docopt import docopt
-from mutagen.easyid3 import EasyID3
-from mutagen.mp3 import MP3
-from prompt_toolkit import prompt
-from prompt_toolkit.shortcuts import print_tokens
-from prompt_toolkit.styles import style_from_dict
-from pygments.token import Token
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='ItunesLibrary 1.0')
@@ -38,4 +31,7 @@ if __name__ == '__main__':
         print("Not implemented yet")
 
     if arguments["modify"]:
-        print("Not implemented yet")
+        library = ituneslibrarian.parse.to_dict(
+            arguments["--input"])
+
+        ituneslibrarian.modify.library(library)
